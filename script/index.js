201a80e9ba1fac74c14fa6376c7d501d01450551
Utpal <2022746@iub.edu.bd>
2026-06-06 21:11:25 +0600

    added modal for for each food

--- script/index.js
+++ script/index.js
@@ -13,6 +13,13 @@ const loadFoods = (id) => {
     .then((data) => displayFoods(data.foods));
 };
 
+const loadFoodDetails = (id) => {
+  const uri = `https://taxi-kitchen-api.vercel.app/api/v1/foods/${id}`;
+  fetch(uri)
+    .then((res) => res.json())
+    .then((data) => displayDetails(data.details));
+};
+
 const displayCategory = (categories) => {
   //   console.log(categories);
   const catContainer = document.getElementById("category-container");
@@ -32,13 +39,36 @@ const displayCategory = (categories) => {
   }
 };
 
+const displayDetails = (food) => {
+  const detailsContainer = document.getElementById("details-container");
+  detailsContainer.innerHTML = `
+     <div class="">
+        <h2 class="text-3xl font-bold">${food.title}</h2>
+        
+      </div>  
+  
+     <div class="">
+        <img src="${food.foodImg}" alt="">
+      </div>
+  
+ 
+      <div class="flex gap-2 my-4 items-center"> <div class="badge badge-primary bg-sky-400 rounded-sm p-2">
+        ${food.area}
+      </div>
+      
+    <a href="${food.video}" target="_blank" class="btn btn-primary p-2 rounded-sm bg-yellow-500">Watch Now</a></div>
+     
+ `;
+  document.getElementById("my_modal_3").showModal();
+};
+
 const displayFoods = (foods) => {
   const foodContainer = document.getElementById("food-container");
   foodContainer.innerHTML = "";
   foods.forEach((food) => {
     const foodCard = document.createElement("div");
     foodCard.innerHTML = `
-     <div
+     <div onclick="loadFoodDetails(${food.id})"
           class="p-5 bg-white flex flex-col md:flex-row gap-3 shadow rounded-xl"
         >
           <div class="img flex-1">
