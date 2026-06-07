787c420e66dbefa36a2a42c3a38f7fa61e84e287
Utpal <2022746@iub.edu.bd>
2026-06-07 20:40:24 +0600

    implement add items in add cart and also showing total price counting

--- script/index.js
+++ script/index.js
@@ -29,6 +29,9 @@ const loadFoodDetails = (id) => {
     .then((data) => displayDetails(data.details));
 };
 
+let cart = [];
+let totalPrice = 0;
+
 const displayCategory = (categories) => {
   const catContainer = document.getElementById("category-container");
   catContainer.innerHTML = "";
@@ -76,19 +79,20 @@ const displayFoods = (foods) => {
   foods.forEach((food) => {
     const foodCard = document.createElement("div");
     foodCard.innerHTML = `
-     <div onclick="loadFoodDetails(${food.id})"
+     <div 
           class="p-5 bg-white flex flex-col md:flex-row gap-3 shadow rounded-xl"
         >
           <div class="img flex-1">
             <img
               src="${food.foodImg}"
               alt=""
-              class="w-full md:w-[160px] rounded-xl h-[220px] md:h-[160px] object-cover"
+              onclick="loadFoodDetails(${food.id})"
+              class="w-full md:w-[160px] rounded-xl h-[220px] md:h-[160px] object-cover food-img"
             />
           </div>
 
           <div class="flex-1">
-            <h1 class="text-xl font-bold">
+            <h1 class="text-xl font-bold food-title">
               ${food.title}
             </h1>
 
@@ -99,13 +103,13 @@ const displayFoods = (foods) => {
             <div class="divider divider-end">
               <h2 class="text-yellow-600 font-semibold">
                 $
-                <span class="price">${food.price}</span>
+                <span class="food-price">${food.price}</span>
                 BDT
               </h2>
             </div>
 
             <button
-              class="btn btn-warning w-full md:w-auto"
+              onclick="addToCart(this)" class="btn btn-warning w-full md:w-auto "
             >
               <i class="fa-solid fa-square-plus"></i>
               Add This Item
@@ -120,3 +124,65 @@ const displayFoods = (foods) => {
 };
 
 loadCategory();
+
+const addToCart = (btn) => {
+  const card = btn.parentNode.parentNode;
+  const foodTitle = card.querySelector(".food-title").innerText;
+  const foodImg = card.querySelector(".food-img").src;
+  const foodPrice = card.querySelector(".food-price").innerText;
+  const foodPriceNum = Number(foodPrice);
+  console.log(foodTitle, foodImg, foodPrice);
+  const selectedItem = {
+    foodTitle: foodTitle,
+    foodImg: foodImg,
+    foodPriceNum: foodPriceNum,
+  };
+  cart.push(selectedItem);
+  totalPrice += foodPriceNum;
+  displayCart(cart);
+  displayTotal(totalPrice);
+};
+
+const displayTotal = (val) => {
+  document.getElementById("total-price").innerText = val;
+};
+
+const displayCart = (cart) => {
+  const cartContainer = document.getElementById("cart-container");
+  cartContainer.innerHTML = "";
+
+  cart.forEach((item) => {
+    const newItem = document.createElement("div");
+    newItem.innerHTML = `
+     <div class="p-1 bg-white flex gap-3 shadow rounded-xl relative">
+            <div class="img">
+              <img
+                src="${item.foodImg}"
+                alt=""
+                class="w-[50px] rounded-xl h-[50px] object-cover"
+              />
+            </div>
+
+            <div class="flex-1">
+              <h1 class="text-xs font-bold">
+                Roast fennel and aubergine paella
+              </h1>
+
+              <div>
+                <h2 class="text-yellow-600 font-semibold">
+                  <span class="price">${item.foodPriceNum}</span>
+                  BDT
+                </h2>
+              </div>
+            </div>
+
+            <div
+              class="w-6 h-6 flex justify-center items-center bg-red-600 rounded-full absolute -top-1 -right-1 text-white cursor-pointer"
+            >
+              <i class="fa-solid fa-xmark"></i>
+            </div>
+          </div>`;
+
+    cartContainer.append(newItem);
+  });
+};
